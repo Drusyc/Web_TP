02bd03251b40d1e86c211c83fb87d93a144f6c86
Thomas <guntho.neo@gmail.com>
2015-02-02 09:37:45 +0100

    here

--- TP_2_JS/monscript.js
+++ TP_2_JS/monscript.js
@@ -5,7 +5,7 @@ function color() {
 
   for (var i = 1; i < rows.length; ++i) {
     if (i % 2 == 0)
-      rows[i].className = "gray"; 
+      rows[i].className = "grayHeader"; 
 
     var cells = rows[i].getElementsByTagName("td");
 // //     al(cells[5]);
@@ -13,7 +13,7 @@ function color() {
 	cells[5].className = "badGrade";
 
     } else if (cells[5].textContent < 10) {
-	cells[5].className = "bofGrade";
+	cells[5].className = "mediocreGrade";
 
     } else if (cells[5].textContent < 12) {
 	cells[5].className = "fairGrade";
@@ -42,9 +42,50 @@ function removeIndex() {
 
 }//removeIndex()
 
+function MrHide () {
+   
+  var table = document.getElementById("T-1");
+  if (table.className.contains("hidden")) {
+    table.classList.remove("hidden"); 
+  } else {
+    table.classList.add("hidden");  
+  }
+}
+
+function isSorted(table, id) {
+  var table = document.getElementById("T-1");
+  var rows = table.getElementsByTagName("tr");
+  var columns = table.getElementsByTagName("th");
+  
+}
+
+function MrTrie () {
+  var id = this.cellIndex;
+  var table = document.getElementById("T-1");
+  var rows = table.getElementsByTagName("tr");
+  var tableData = [];
+
+  for (var i = 1; i < rows.length; ++i) {
+    var cells = rows[i].getElementsByTagName("td");
+    tableData[i] = cells[id].textContent;
+  }
+
+  //isSorted(tableData);
+}
+
 function main () {
+
   color();
   removeIndex();
+
+  //Cacher les notes\\
+  document.getElementsByTagName("h1")[0].addEventListener("click", MrHide, false);
+
+  //Trier les notes\\
+  var columns = document.getElementById("T-1").getElementsByTagName("th");
+  for (var i = 0; i < columns.length; ++i)
+    columns[i].addEventListener("click", MrTrie, false);
 }
 
 window.addEventListener("load", main, false);
+
