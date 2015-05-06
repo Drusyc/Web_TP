950705681c7ee3bda6444bf81a889bab2cdb2d23
Thomas <guntho.neo@gmail.com>
2015-05-06 16:06:26 +0200

    voilà

--- TP_3/jetty/webapps/tpajax/WEB-INF/classes/EnsimagServlet.java
+++ TP_3/jetty/webapps/tpajax/WEB-INF/classes/EnsimagServlet.java
@@ -1,5 +1,9 @@
 import java.io.*;
 import java.net.*;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.*;
 import java.util.regex.*;
 
@@ -22,6 +26,12 @@ public class EnsimagServlet extends HttpServlet {
 	private Element cours;
 	// L'attribut p est un objet initialisé à la création décrivant sous la forme d'une expression régulière le format d'une ligne décrivant un cours
 	private Pattern p;
+
+	//Informations relatives à la base de données h2 (utilisées par la fonction getConnection)
+	private final String jdbcUrl = "jdbc:h2:tcp://localhost/~/test";
+	private final String jdbcUser = "guntzt";
+	private final String jdbcPassword = "howsecureisthispassword";
+	
 	
 	//Lors de la première invocation de la Servlet la méthode init est invoquée, ici sont initialisées les attributs
 	public void init() throws ServletException 
@@ -50,19 +60,21 @@ public class EnsimagServlet extends HttpServlet {
 		Matcher m = null;
 		Elements rows = cours.getElementsByTag("tr");
 		String result = "";
+		int nbResult = 0;
 		
 		if (!recherche.isEmpty()) {
-			for (int i = 2; i < rows.size()-1; i++) {
+			for (int i = 2; i < rows.size()-1 && nbResult < 10; i++) {
 		
 				Elements cells = rows.get(i).getElementsByTag("td");
 				
 				if (cells.size() > 2){
+					
 					m = p.matcher((cells.get(0).text()));
 					
 					String strApo 	= "\"apogee\":\"";
 					String strNom 	= "\"nom\":\"";
 					String strEcts 	= "\"ects\":\"" + cells.get(1).text() + "\"";
-					String strUrl 	= "\"url\":\"" + cells.get(0).getElementsByTag("a").get(0).attributes().get("href") + "\""; //EASY
+					String strUrl 	= "\"url\":\"" + cells.get(0).getElementsByTag("a").get(0).attributes().get("href") + "\"";
 					while (m.find()) {
 						strApo 	+= m.group(1) + "\"";
 						strNom  += m.group(2) + "\""; 
@@ -71,10 +83,12 @@ public class EnsimagServlet extends HttpServlet {
 					if(strApo.toLowerCase().contains(recherche.toLowerCase()) 
 						|| strNom.toLowerCase().contains(recherche.toLowerCase()) 
 						|| strEcts.toLowerCase().contains(recherche.toLowerCase()) 
-						|| strUrl.toLowerCase().contains(recherche.toLowerCase()))			
+						|| strUrl.toLowerCase().contains(recherche.toLowerCase())) {	
 						result += "{" + strApo + "," + strEcts + "," + strNom + "," + strUrl + "},\n";
-				}
-			}
+						nbResult++;
+					}
+				}//if cells.size>2
+			}//for()
 			if (result.length() > 2) 
 				out.print(result.substring(0, result.length() - 2));
 		}
@@ -89,6 +103,7 @@ public class EnsimagServlet extends HttpServlet {
 		resp.setContentType("/application/json; charset=UTF8");
 		PrintWriter out=resp.getWriter();
 		
+		/* urlMatiere ne contient que la fin de l'url, il faut rajouter le début (adresse de l'ensimag) et concater la fin. */
 		Connection con = Jsoup.connect("http://ensimag.grenoble-inp.fr/cursus-ingenieur/" + urlMatiere).userAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.21 (KHTML, like Gecko) Chrome/19.0.1042.0 Safari/535.21").timeout(10000);
 		Connection.Response response = con.execute();
 		//Document doc = Jsoup.connect("http://ensimag.grenoble-inp.fr/cursus-ingenieur/" + urlMatiere).get();
@@ -98,6 +113,8 @@ public class EnsimagServlet extends HttpServlet {
         Document doc = con.get();
 		Element page = doc.getElementById("contenus_page");
 		Element objectifs = null;
+		
+		/* Récupère les objectifs contenus dans un <div class="hsep element_deco">, fils de l'element page */
 		for (int i = 0; i < page.children().size(); i++) {
 			if (page.child(i).hasClass("hsep")) {
 				objectifs = page.child(i);
@@ -106,8 +123,13 @@ public class EnsimagServlet extends HttpServlet {
 		
 		out.print("[");
 		out.print("{\"Objectifs\" : \"");
+		
+		/* Split le texte de l'Objectifs par rapport au caractère '"' 
+		 * Le JSON n'aime pas avoir des '"' en trop dans les valeurs des éléments.. */
 		String[] sub = objectifs.text().split("\"");
 		for (int i = 0; i < sub.length; i++) {
+			
+			/* On retire le premier mot "Objectifs" (si il y est) */
 			if (i == 0 && (sub[i].substring(0, "Objectifs".length()).equals("Objectifs"))) {
 				out.print(sub[i].substring("Objectifs".length(), sub[i].length()-1));
 			} else {
@@ -118,17 +140,43 @@ public class EnsimagServlet extends HttpServlet {
 		out.print("]");
 		}
 		catch (HttpStatusException e) {
-	        System.out.println("URL incorrecte : " + e.getUrl());
+	        //Adresse incorrecte..
+	        System.out.println(e);
 		}
 		catch(Exception e ) {System.out.println(e);e.printStackTrace();}		
 	}
 	
+	/*
+	 * Renvoie le dernier commentaire de la table COMMENTAIRES 
+	 */
+	private void getLastComment(HttpServletResponse resp,String code) throws ServletException, IOException {
+		resp.setContentType("/application/json; charset=UTF-8");
+		//resp.setContentType("/text/html; charset=UTF-8");
+		PrintWriter out=resp.getWriter();	
+				
+		/* Vérification des paramètres */
+		Boolean isReqCorrect = checkCommentairesArg(out, code, null, null, null);
+		if (!isReqCorrect) {
+			return;
+		}//check
+		
+		java.sql.Connection conn;
+		try {
+			conn = getConnection();
+			int rset = selectCommentFromCode(conn,out, code, "json");
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
 			IOException {
 			//Récupère le chemin dans l'URL
 			try {
+			System.out.println("GET" + req.toString());
 			String [] valeurs=req.getRequestURI().substring(req.getContextPath().length()+1).split("/");
 			
 			if (valeurs.length==2){
@@ -139,9 +187,244 @@ public class EnsimagServlet extends HttpServlet {
 				else if ("objectifs".equals(valeurs[0])){
 					getObjectifs(resp,parametre);				
 				}
+				else if ("commentaires".equals(valeurs[0])){
+					getLastComment(resp,parametre);
+				}
 			}
 			
 			}
 			catch (Exception e ){System.out.println(e);e.printStackTrace();}
+	}
+	
+	/* Fonction renvoyant une connexion vers la base de donnée h2 */
+	private java.sql.Connection getConnection() throws SQLException {
+		java.sql.Connection conn = null;
+		try {
+			Class.forName("org.h2.Driver");
+			conn = DriverManager.getConnection(jdbcUrl, jdbcUser, jdbcPassword);
+			//conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/mybd", "guntzt", "howsecureisthispassword");
+		    System.out.println("Connected to database");
+		    
+		    
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Error database");
+			e.printStackTrace();
+		}
+		return conn;
+	}//getConnection()
+
+	/*
+	 * Fonction envoyant une requête de type INSERT INTO à la table COMMENTAIRES 
+	 */ 
+	private int insertIntoCommentaires(java.sql.Connection conn, String codeMat, String mail, Integer age, String comment) {
+		/* Code de la matière */
+		final int indexCode = 1;
+		/* mail du redacteur */
+		final int indexMail = 2;
+		/* Age du redacteur */
+		final int indexAge = 3;		
+		/* Commentaires du redacteur */
+		final int indexComment = 4;	
+		
+		int rs = -1;
+		
+		/* PreparedStatement pour insérer un commentaire */
+		PreparedStatement statement;
+		try {
+			statement = conn.prepareStatement("INSERT INTO COMMENTAIRES (CODE, CDATE, REDACTEUR, AGE, COMMENTAIRES) "
+															  + "VALUES(?, CURRENT_TIMESTAMP, ?, ?, ?);");
+			statement.setString(indexCode, codeMat);
+			statement.setString(indexMail, mail);
+			statement.setInt(indexAge, age);
+			statement.setString(indexComment, comment);
+			
+			rs = statement.executeUpdate();			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		return rs;
+	}
+	
+	/* 
+	 * Fonction envoyant une requête SELECT * à la table COMMENTAIRES.
+	 * codeMat est le code sur lequel effectuer la requête
+	 * typeReturn peut être soit "html" soit "json", définie sous quelle forme le serveur renvoie les résultats de la requête 
+	 */
+	private int selectCommentFromCode (java.sql.Connection conn,PrintWriter out, String codeMat, String typeReturn) {
+		ResultSet rset = null;
+		
+		/* Index code */
+		final int indexCode = 1;
+		PreparedStatement statementCheck, statementSelect;
+		try {
+			
+//			statementCheck = conn.prepareStatement("SELECT DISTINCT CODE FROM COMMENTAIRES WHERE CODE = ?;");
+//			statementCheck.setString(indexCode, codeMat);
+//			rset = statementCheck.executeQuery();			
+			
+			statementSelect = conn.prepareStatement("SELECT * FROM COMMENTAIRES WHERE CODE = ? ORDER BY CDATE DESC;");
+			statementSelect.setString(indexCode, codeMat);
+			rset = statementSelect.executeQuery();
+			
+			if ("html".equals(typeReturn)) {
+				String htmlResponse = "";
+				int cpt = 1;
+				while(rset.next()) {
+					System.out.println(rset.toString());
+					htmlResponse += "<li id=\"com_" + cpt + "\">";
+					htmlResponse += rset.getNString("REDACTEUR") + " a écrit le [" + rset.getDate("CDATE", Calendar.getInstance()) + "] : <br><br>"; 				
+					htmlResponse += "<em>" + rset.getNString("COMMENTAIRES") + "</em> <br><br>" ;
+					htmlResponse += "</li>";
+					
+					cpt++;
+				} 
+				
+				out.println(htmlResponse);
+			} else if ("json".equals(typeReturn)) {
+				out.print("[");
+				out.print("{");
+				
+				/* Si aucun résultat .. */
+				if (!rset.next()) {
+					out.print("\"nbComment\":\"" + "0" + "\"");
+					out.print("}");
+					out.print("]");
+					return 0;
+				}
+				
+				/* Récupère le dernier commentaire posté */
+
+				out.print("\"redacteur\":\"" + rset.getNString("REDACTEUR") + "\",");
+				out.print("\"date\":\"" + rset.getDate("CDATE", Calendar.getInstance()) + "\",");
+				out.print("\"commentaires\":\"" + rset.getNString("COMMENTAIRES") + "\",");
+				
+				/* Récupère le nombre de commentaires */
+				rset.last();
+				out.print("\"nbComment\":\"" + rset.getRow() + "\"");
+				
+				out.print("}");
+				out.print("]");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		return 0;
+	}
+	
+	
+	
+	/* Fonction de vérifications des valeurs données pour la table COMMENTAIRES 
+	 * Renvoie false si un élément ne respecte pas la forme attendue 
+	 * La fonction peut être utilisée juste pour tester l'attribut CODE (utile pour les requêtes de type SELECT.. WHERE CODE = ?)
+	 * Pour cela, passer simplement codeMat et mettre les autres à null (mail, age, comment)
+	 */
+	private Boolean checkCommentairesArg(PrintWriter out, String codeMat, String mail, Integer age, String comment) {
+		
+		Pattern p;
+		Matcher m;
+		
+		/* Check codeMat */
+		p = Pattern.compile("[a-zA-Z0-9]{5,10}+");
+		m = p.matcher(codeMat);
+		if (!m.find()) {
+			out.println("<li id=\"error\" style=\"color:red\">Ce code ne correspond pas au format attendu !</li>");
+			return false;
+		}//fi condi codeMat
+		
+		
+		if (mail != null) {
+			/* Check email */    /*    abz7ab         (.ab89a)         @ ab89bb          .fr */
+			p = Pattern.compile("^[A-Za-z_0-9]+(\\.[A-Za-z_0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z]{2,5})$");
+			m = p.matcher(mail);
+			if (!m.find()) {
+				out.println("<li id=\"error\" style=\"color:red\">L'email ne correspond pas au format attendu !</li>");
+				return false;
+			}//fi condi mail
+		}
+		
+		if (age != null) {
+			/* Check age */
+			if (age < 18 || age > 100) {
+				out.println("<li id=\"error\" style=\"color:red\">Vous devez rentrer un âge entre 18 et 100ans!</li>");
+				return false;
+			}//fi condi age
+		}
+		
+		if (comment != null) {
+			/* Check commentaires */
+			if (comment.length() > 500) {
+				out.println("<li id=\"error\" style=\"color:red\">Vos commentaires ne doivent "
+						+ "pas excéder les 500 caractères (actuels : "+ comment.length() + "!)</li>");
+				return false;
+			}//fi condi taille commentaires 
+			p = Pattern.compile("[;_§\\{\\}\\|]");
+			m = p.matcher(comment);
+			if (m.find()) {
+				out.println("<li id=\"error\" style=\"color:red\">Le commentaire ne correspond pas au format attendu !</li>");
+				return false;
+			}//fi condi mail
+		}
+		return true;
+	}
+	
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException {
+		try {
+			
+			String [] valeurs=req.getRequestURI().substring(req.getContextPath().length()+1).split("/");
+			
+			if (valeurs.length==2){
+				String parametre=URLDecoder.decode(valeurs[1],"UTF-8");
+				
+				if ("select".equals(parametre)) {
+					/* ** Récupération des données du POST ** */
+					String codeMat = req.getParameter("codeMatiere");
+					resp.setContentType("text/html;charset=UTF-8");
+					PrintWriter out=resp.getWriter();			
+					
+					/* Vérification des paramètres */
+					Boolean isReqCorrect = checkCommentairesArg(out, codeMat, null, null, null);
+					if (!isReqCorrect) {
+						return;
+					}//check
+					
+					java.sql.Connection conn = getConnection();
+					int rset = selectCommentFromCode(conn,out, codeMat, "html");
+					conn.close();
+					
+				} else if ("insert".equals(parametre)) {
+					/* ** Récupération des données du POST ** */
+					String codeMat = req.getParameter("codeMatiere");
+					String mail = req.getParameter("mailRedacteur");
+					Integer age = Integer.parseInt(req.getParameter("ageRedacteur"));
+					String comment = req.getParameter("commentaires");
+					/* ** ** */
+					
+					resp.setContentType("text/html;charset=UTF-8");
+					PrintWriter out=resp.getWriter();			
+					
+					/* Vérification des paramètres */
+					Boolean isReqCorrect = checkCommentairesArg(out, codeMat, mail, age, comment);
+					if (!isReqCorrect) {
+						return;
+					}//check
+					
+					java.sql.Connection conn = getConnection();
+					int rs = insertIntoCommentaires(conn, codeMat, mail, age, comment);
+					System.out.println("Statement result : " + rs);
+					
+					int rset = selectCommentFromCode(conn,out, codeMat, "html");
+					conn.close();
+				}
+			}
+			
+			System.out.println("POST" + req.toString());
+			
+		}
+		catch (Exception e ){System.out.println(e);e.printStackTrace();}
 	}
 }
--- TP_3/jetty/webapps/tpajax/javascript/ajax.js
+++ TP_3/jetty/webapps/tpajax/javascript/ajax.js
@@ -1,4 +1,10 @@
-/* Fonction de w3school */
+/* Ouvre un nouvel onglet pour afficher le formulaire et les commentaires */
+function formComment (matiere) {
+  //window.location.assign("formulaire.html");
+  window.open("commentaires.html", matiere);
+}
+
+/* Fonction de w3school pour la gestion du cookie */
 function setCookie (cname, cvalue, exdays) {
   var d = new Date();
   d.setTime(d.getTime() + (exdays*24*60*60*1000));
@@ -6,7 +12,7 @@ function setCookie (cname, cvalue, exdays) {
   document.cookie = cname + "=" + cvalue + "; " + expires;  
 }
 
-/* Fonction de w3school */
+/* Fonction de w3school pour la gestion du cookie */
 function getCookie(cname) {
   var name = cname + "=";
   var ca = document.cookie.split(';');
@@ -18,15 +24,19 @@ function getCookie(cname) {
   return "";
 }
 
-function recherche() {
 
+/* Fonction appelée lorsqu'une lettre est tapée dans le champ input 
+   Effectue une requête sur le serveur et affiche les résultats */
+function recherche() {
   var url = "http://localhost:8080/tpajax/recherche/";
   var xmlhttp = new XMLHttpRequest();
   
+  /* Fonction appelée lorqu'on reçoit une réponse du serveur */
   xmlhttp.onreadystatechange = function() {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
       $("#resultats").remove();
       
+    /* Petit header pour afficher le nombre de résultats */
       var msgResultat;
       if (document.getElementsByTagName("h2").length == 1) 
         msgResultat = document.getElementsByTagName("h2")[0];
@@ -38,17 +48,11 @@ function recherche() {
         msgResultat.innerHTML = resultats.length + " résultat(s) pour \"" + $("#recherche").val() + "\"";
         $("body").append(msgResultat);
 
+        /* Affiche les résultats du JSON*/
         afficheResultat(resultats);
       } else {
         msgResultat.innerHTML = "";
       }
-/*
-      
-
-
-      if (xmlhttp.responseText.length > 0) {
-        afficheResultat(resultats);
-      }*/
     }
   }
   xmlhttp.open("GET", url.concat($("#recherche").val()), true);
@@ -57,29 +61,48 @@ function recherche() {
   setCookie("cookieRecherche",$("#recherche").val(), 1);  
 }
 
+/* Récupère la réponse du serveur (JSON) au format text et affiche le contenu de la recherche */
 function afficheResultat(res) {
-
-
-
     var liste = document.createElement("ul");
     liste.setAttribute("id", "resultats");
     liste.setAttribute("style", "list-style-type:none");
-
 
     for(var i = 0; i < res.length; i++) {
       var item = document.createElement("li");
       var htmllink = document.createElement("a");
 
       item.setAttribute("id", "resultats_" + i);
-      //item.setAttribute("class", "resultat");
 
-      htmllink.style.color = (i%2 == 0) ? "#006666" : "#009966" ;
-  
+      //htmllink.style.color = (i%2 == 0) ? "#006666" : "#009966";
+      if (i%2 == 0)
+        htmllink.setAttribute("style", "color:#006666");
+      else 
+        htmllink.setAttribute("style", "color:#009966");
+
+
       htmllink.setAttribute("href", res[i].url);
       htmllink.setAttribute("class", "resultat");
       htmllink.innerHTML = res[i].apogee + " : " + res[i].nom;
-    
+                    
+      var details = document.createElement("details");
+      details.setAttribute("id", "details_" + "resultats_" + i);
+      details.classList.add("objAndComm");
+
+      
+//       Codant sous Firefox (avec les machines virtuelles de l'ensimag), je n'ai pas pu tester le bon fonctionnement 
+//       des éléments <details>. J'ai donc opté pour masquer/afficher les informations en passant la souris sur le lien du cours.
+//       J'ai retiré le <summary> qui ne se comportait bizarrement avec cette option d'affichage/masquage.
+
+//       var sum = document.createElement("summary");
+//       sum.innerHTML = "Détails du cours (Objectifs et Commentaires)";
+// 
+//       details.appendChild(sum);
+      
+
+      item.appendChild(details);
+
       item.appendChild(htmllink);
+
       liste.appendChild(item);
     }
     
@@ -87,20 +110,34 @@ function afficheResultat(res) {
     
     /* Event pour récupérer les objectifs */
     $("a.resultat").bind("mouseenter", getObjectifs);
-    $("a.resultat").bind("mouseout", toggleObjectifs);
+
+    /* Cache les objectifs une fois que la souris sort du l'hypertext */
+    //$("a.resultat").bind("mouseout", toggleObjectifs);
 }
 
+/* Fonction appelé lorsque la souris est passée sur un lien 
+   Effectue une requête sur le serveur pour récupérer les objectifs du cours souhaités 
+   Affiche les objectifs */
 function getObjectifs() {
 
   /* La ligne correspondant de la liste */
   var li = this.parentElement;
 
-  /* Si la recherche a déjà été faite, elle a été caché, on la réaffiche.
+  /* Si la recherche a déjà été faite, elle a été cachée, on la réaffiche.
      Evite une nouvelle requête au serveur */
   if (li.getElementsByClassName("objectifs").length > 0) {
-    $("#objectif_" + li.getAttribute("id")).show("fast");
+    if ($("#details_" + li.getAttribute("id")).attr("style") == "display: none;") {
+      $("#details_" + li.getAttribute("id")).show("fast");
+      
+      /* On refait la requête REST (au cas où un commentaire ait été rajouté entre temps) */
+      restRequest(this.textContent, li);
+    } else {
+      $("#details_" + li.getAttribute("id")).hide("fast");
+    }
     return;
   } 
+
+
   var url = "http://localhost:8080/tpajax/objectifs/";
 
   /* Récupère l'adresse de la page de présentation de la matière */
@@ -109,10 +146,12 @@ function getObjectifs() {
   /* Découpe le lien */
   var values = urlObj.split("/");
 
+   /* Fonction appelée lorqu'on reçoit une réponse du serveur */
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function() {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
       if (xmlhttp.responseText.length > 0) {
+        /* Affiche le résultat */
         afficheObjectifs(JSON.parse(xmlhttp.responseText), li.getAttribute("id"));
       }
     }
@@ -120,34 +159,194 @@ function getObjectifs() {
   xmlhttp.open("GET", url.concat(values[values.length-1]), true);
   xmlhttp.send();
   
-}
 
+  /* REST requête */
+  restRequest(this.textContent, li);
+}//getObjectifs()
+
+
+
+/* Cache les objectifs (*/
 function toggleObjectifs () {
   if (this.parentElement.getElementsByClassName("objectifs").length > 0) 
     $(document).ready($("#objectif_" + this.parentElement.getAttribute("id")).hide("fast"));   
-}
+}//toggleObjectifs()
 
+
+
+
+/* Récupère la réponse du serveur (JSON) au format text et affiche les objectifs */
 function afficheObjectifs(responseObjectifs, IDListe) {
   
-  var item = document.createElement("p");
-  item.setAttribute("id", "objectif_" + IDListe);
+  var item = document.getElementById("details_"+IDListe);
+  var para = document.createElement("p");
+  item.appendChild(para);
+  para.setAttribute("id", "objectif_" + IDListe);
   item.classList.add("objectifs");
 
-  item.innerHTML = responseObjectifs[0].Objectifs;
+  para.innerHTML = "Objectifs du cours : <br>" + responseObjectifs[0].Objectifs;
 
   $("#"+IDListe).append(item);
 
   $(document).ready($("#objectif_" + IDListe).show("fast"));
-}
+}//afficheObjectifs()
+
+
+
+/* Fonction de vérification pour le champ textarea de commentaires.html */
+function checkInputTxt() {
+  var values = $("#inputTxt").val();
+  var cpt = 0;
+  var re = /^[\w\d\s,:!&"'()*$?.%]$/;
+
+  /* Ne sachant pas trop comment interdire un caractère tapé, je remets à jour directement la value de textarea */
+  var correctStr = new String("");
+
+  for(var i = 0; i < values.length; i++) {
+    if (re.test(values[i])) {
+      correctStr = correctStr.concat(values[i]);
+      cpt++;
+    }
+  }//for()
+
+  $("#labelInputTxt").html("Texte (" + cpt + "/500):");
+  $("#inputTxt").val(correctStr.toString());
+}//checkInputTxt()
+
+
+
+/* Fonction qui effectue la requête au serveur pour récupérer le dernier commentaire */
+function restRequest (matiere, element) { 
+  
+  var splitted = matiere.split(" :");
+  var code = splitted[0];
+
+  /* Création de l'élément qui va contenir le dernier commentaire + un bouton (lien) vers commentaires.html */
+  if (document.getElementById("commentaires_" + code) != null)
+    var item = document.getElementById("commentaires_" + code);
+  else {
+    var item = document.createElement("p");
+    item.setAttribute("id", "commentaires_" + code);
+    item.classList.add("lastCommentaires");
+
+    /* Rattachement vers l'élement du dessus */
+    li = document.getElementById("details_" + element.getAttribute("id"));
+    li.appendChild(item);
+    
+    /* Création du bouton (lien) vers commentaires.html (le bouton envoie comme données le titre du cours et le code) */
+    var formu = document.createElement("form");
+    //formu.setAttribute("method", "get");
+    formu.setAttribute("id", "formOpenComment");
+    formu.setAttribute("action", "javascript: formComment(" + "\"" + matiere + "\""  + ")");
+  
+    var b = document.createElement("button");
+    b.setAttribute("type", "submit");
+    b.setAttribute("name", "Cours");
+    b.setAttribute("value", matiere);
+    b.innerHTML = "Commentaires";
+
+    formu.appendChild(b);
+    li.appendChild(formu);
+  }
+
+  /* Requête REST json */
+  $.getJSON("http://localhost:8080/tpajax/commentaires/" + code, function(result){
+      var str = "<strong>Nombre de commentaire(s) pour ce cours:  </strong>";
+      $.each(result, function(i, field){
+          str += field.nbComment + "<br><br>";
+
+          if (field.nbComment != "0") {
+            str += "<strong>Dernier commentaire déposé par:</strong> " + field.redacteur  + "<br><br>";
+            str += "<em>\"" + field.commentaires + "\"</em><br><br>";
+            str += "<small>Commentaire ajouté le " + field.date + "</small>";
+          }
+          item.innerHTML =  str ;
+      });
+  });  
+}//restRequest()
+
+
+
+/* Function appelée par le formulaire et le bouton F5 de commentaires.html 
+   Effectue des requêtes au serveurs (SELECT ou un INSERT TO) */
+function getPostResult(arg) {
+  if (arg == "insert") {
+    $.post("http://localhost:8080/tpajax/commentaires/insert", 
+          {
+              mailRedacteur : $("#inputMail").val(),
+              codeMatiere   : $("#inputCode").val(),
+              ageRedacteur  : $("#inputAge").val(),
+              commentaires  : $("#inputTxt").val()            
+            }
+            , function(data,status) {
+                $("#listComment").html(data);
+            });
+  } else if (arg == "select"){
+    $.post("http://localhost:8080/tpajax/commentaires/select", 
+          {
+              codeMatiere   : $("#inputCode").val(),        
+            }
+            , function(data,status) {
+                $("#listComment").html(data);
+            });
+  }
+}//getPostResult()
+
+
+
+
+/* Fonction d'initialisation de la page commentaires.html 
+   Met à jours les éléments du formulaires et leur évenements */
+function setUpForm() {
+  /* Récupère l'info de la matière passée dans window.name */
+  infoMatiere = window.name.split(" :"); // 0 : code // 1 : nom
+
+  /* Affiche les commentaires de la base */
+  //afficheCommentaires(infoMatiere[0]);
+
+  /* Le header h1 récupère l'info du nom de la matière */
+  $("h1").html("Commentaires de : " + infoMatiere[1]);
+
+
+  /* On set l'input "Code" et on le rend inclickable */
+  $("#inputCode").val(infoMatiere[0]);
+  $("#inputCode").attr("style", "background-color:#008B8B");
+  $("#inputCode").focus(function() {
+    $(this).blur();
+  });
+
+  /* Evenement de l'input textarea (check les caractères ajoutés) */
+  $("#labelInputTxt").html("Texte (0/500):");
+  $("#inputTxt").bind("keyup", checkInputTxt);
+
+  /* Bouton F5 pour donner les commentaires sans être obligé d'en ajouter un */
+  $("#buttonSelect").attr("onclick","javascript:getPostResult('select')");
+
+
+  /* Action à lancée une fois submit */
+  $("#comForm").attr("action", "javascript:getPostResult('insert')");
+  $("#comForm").submit(function(event) {
+    if ( $("#inputTxt").val() != "") { 
+      return;
+    }//fi
+    alert("Ecrivez au moins un mot ! :)");
+    event.preventDefault();
+  });//checkForm         
+}//setUpForm()
 
 function main() {
-  /* Si l'utilisateur a déjà effectué une recherche,
-     on récupère cette recherche par le cookie et le met en valeur de l'input */
-  $("#recherche").attr("value", getCookie("cookieRecherche"));
 
-  /* Déclenche une requête pour le serveur dès qu'une lettre est tapée */
-  $("#recherche").bind("keyup", recherche);
- 
+  if (document.title == "Ensimaagle") {
+    /* Si l'utilisateur a déjà effectué une recherche,
+      on récupère cette recherche par le cookie et le met en valeur de l'input */
+    $("#recherche").attr("value", getCookie("cookieRecherche"));
+
+    /* Déclenche une requête pour le serveur dès qu'une lettre est tapée */
+    $("#recherche").bind("keyup", recherche);
+  
+  } else if (document.title == "Commentaires") {
+    setUpForm();    
+  }//fi
 }
 
 $(window).bind("load",main);
